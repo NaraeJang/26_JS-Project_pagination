2b775707a0a85d306287db8459e66b0003733cba
NaraeJang <j.naaaarae@gmail.com>
2023-08-02 22:42:12 -0400

    button functionality

--- app.js
+++ app.js
@@ -24,4 +24,25 @@ const init = async () => {
   setupUI();
 };
 
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-container")) return;
+  if (e.target.classList.contains("page-btn")) {
+    // change string to number.
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+
+  setupUI();
+});
 window.addEventListener("load", init);
